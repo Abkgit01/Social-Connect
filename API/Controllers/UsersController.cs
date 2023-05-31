7a4cb8c43d19eae8f2eaaf9d2c0be7602e2cbb33
Abubakar Ibrahim <abubakar.ibrahim@brandonetech.com>
2023-05-31 23:42:37 +0100

    first commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("GetUsers")]
+        public async Task<IActionResult> GetUsers()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpGet("GetUser/id")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            return Ok(await _context.Users.FindAsync(id));
+        }
+    }
+}
---
+++ API/Models/AppUser.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Models
+{
+    public class AppUser
+    {
+        #region Class Properties
+
+        public int Id { get; set; }
+        public string UserName { get; set; }
+
+        #endregion
+    }
+}
